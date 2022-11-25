d8d11d6421ca8f14f03ba0823d1e745718d4ef75
David Green <dave@activelylazy.co.uk>
2022-11-25 08:06:31 +0000

    Add pdf2json tool and generate json for test file

---
+++ tools/pdf2json.js
@@ -0,0 +1,30 @@
+const { PdfReader } = require('pdfreader');
+const fs = require('fs');
+
+if (process.argv.length <= 2) {
+    throw new Error('Expected command line arguments');
+}
+const filename = process.argv[2];
+const outputName = filename.substr(0, filename.lastIndexOf(".")) + ".json";
+
+function processItems(items) {
+    let json = "";
+    items.forEach(item => {
+        if (json == "") {
+            json += "[\n"
+        } else {
+            json += ",\n"
+        }
+        json += JSON.stringify(item, null, 2);
+    });
+    json += "\n]\n";
+    fs.writeFileSync(outputName, json);
+}
+
+console.log(`Processing ${filename} into ${outputName}`);
+const items = [];
+new PdfReader().parseFileItems(filename, (err, item) => {
+    if (err) { /*console.error("error:", err);*/ }
+    else if (!item) processItems(items);
+    else if (item.text) items.push(item);
+});
