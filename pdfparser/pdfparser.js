d3b2344af1a317691adde22ccd93c19901eaf69a
David Green <dave@activelylazy.co.uk>
2022-11-29 17:54:57 +0000

    Add linting and fix errors

--- pdfparser/pdfparser.js
+++ pdfparser/pdfparser.js
@@ -1,215 +1,217 @@
 import { PdfReader } from 'pdfreader';
 
-var numberOfColumns = 9;
-
 function parse(filename) {
-    let items = [];
-    return new Promise((resolve, reject) => {
-        new PdfReader().parseFileItems(filename, (err, item) => {
-            if (err) { /*console.error("error:", err);*/ }
-            else if (!item) resolve(items);
-            else if (item.text) items.push(item);
-        });
+  const items = [];
+  return new Promise((resolve) => {
+    new PdfReader().parseFileItems(filename, (err, item) => {
+      if (err) { /* console.error("error:", err); */ } else if (!item) resolve(items);
+      else if (item.text) items.push(item);
     });
+  });
 }
 
-function parseDriverLaps(filename) {
-    return parse(filename)
-        .then(items => {
-            const doc = {
-                raceTitle: items[0].text,
-                raceDate: items[1].text,
-                raceTitle2: items[2].text,
-                location: items[3].text,
-                doc: items[4].text,
-                drivers: [],
-            };
+function readDriverHeader(items, startIndex) {
+  let index = startIndex;
+  const driverName = items[index++].text.trim();
+  const driverNumber = Number(items[index++].text.trim());
+  const driverClass = items[index++].text.trim();
 
-            let index = 5;
-            while (index < items.length) {
-                const [ driver, indexAfterDriverHeader ] = readDriverHeader(items, index);
-                index = indexAfterDriverHeader;
+  // todo check these
+  const c1lap = items[index++];
+  if (c1lap.text !== 'lap') {
+    throw new Error(`Expected lap column header but got ${JSON.stringify(c1lap)}`);
+  }
+  const c1s1Header = items[index++].text; // eslint-disable-line no-unused-vars
+  const c1s2Header = items[index++].text; // eslint-disable-line no-unused-vars
+  const c1s3Header = items[index++].text; // eslint-disable-line no-unused-vars
+  const c1laptimeHeader = items[index++].text; // eslint-disable-line no-unused-vars
+  const c2lap = items[index++];
+  if (c2lap.text !== 'lap') {
+    throw new Error(`Expected lap column header but got ${JSON.stringify(c2lap)}`);
+  }
+  const c2s1Header = items[index++].text; // eslint-disable-line no-unused-vars
+  const c2s2Header = items[index++].text; // eslint-disable-line no-unused-vars
+  const c2s3Header = items[index++].text; // eslint-disable-line no-unused-vars
+  const c2laptimeHeader = items[index++].text; // eslint-disable-line no-unused-vars
 
-                const [ driverItems, indexAfterDriverItems ] = readDriverItems(items, index);
-                index = indexAfterDriverItems;
-
-                driver.laps = convertDriverItemsIntoLaps(driverItems, driver);
-                doc.drivers.push(driver);
-                console.log(`Successfully added ${driver.name} with ${driver.laps.length} laps`);
-            }
-        
-            return doc;
-        });
-}
-
-function readDriverHeader(items, index) {
-    let driverName = items[index++].text.trim();
-    let driverNumber = Number(items[index++].text.trim());
-    let driverClass = items[index++].text.trim();
-
-    // todo check these
-    let c1lap = items[index++];
-    if (c1lap.text !== 'lap') {
-        throw new Error(`Expected lap column header but got ${JSON.stringify(c1lap)}`);
-    }
-    let c1s1Header = items[index++].text;
-    let c1s2Header = items[index++].text;
-    let c1s3Header = items[index++].text;
-    let c1laptimeHeader = items[index++].text;
-    let c2lap = items[index++];
-    if (c2lap.text !== 'lap') {
-        throw new Error(`Expected lap column header but got ${JSON.stringify(c2lap)}`);
-    }
-    let c2s1Header = items[index++].text;
-    let c2s2Header = items[index++].text;
-    let c2s3Header = items[index++].text;
-    let c2laptimeHeader = items[index++].text;
-    
-    return [ {
-        name: driverName,
-        number: driverNumber,
-        category: driverClass,
-        columnHeaders: [
-            {
-                lap: {
-                    x: c1lap.x,
-                },
-            },
-            {
-                lap: {
-                    x: c2lap.x,
-                },
-            }
-        ]
-    }, index ];
-}
-
-function readPageHeader(items, index) {
-    const eventName = items[index++].text;
-    const eventDate = items[index++].text;
-    const eventName2 = items[index++].text;
-    const eventLocation = items[index++].text;
-    const documentName = items[index++].text;
-    return [ {
-        eventName,
-        eventDate,
-        eventName2,
-        eventLocation,
-        documentName,
+  return [{
+    name: driverName,
+    number: driverNumber,
+    category: driverClass,
+    columnHeaders: [
+      {
+        lap: {
+          x: c1lap.x,
         },
-        index
-        ];
+      },
+      {
+        lap: {
+          x: c2lap.x,
+        },
+      },
+    ],
+  }, index];
 }
 
-function readDriverItems(items, index) {
-    let driverItems = [];
-    let driverY = items[index].y;
-    while(index < items.length) {
-        let nextItem = items[index];
-
-        if (nextItem.y < driverY) {
-            // page break
-            driverY = nextItem.y;
-            const result = readPageHeader(items, index);
-            const pageHeader = result[0];
-            index = result[1];
-            nextItem = items[index];
-        }
-
-        const nextItemCode = nextItem.R[0].S;
-        if (nextItemCode == -1) {
-            // next driver
-            break;
-        } else if (nextItemCode == 2) {
-            // skip
-            index++;
-            continue;
-        } else if (nextItemCode != 51 && nextItemCode != 52) {
-            throw new Error(`Unexpected driver item code in ${JSON.stringify(nextItem)}`);
-        }
-        driverItems.push(nextItem);
-        index++;
-    }
-    return [ driverItems, index ];
+function readPageHeader(items, startIndex) {
+  let index = startIndex;
+  const eventName = items[index++].text;
+  const eventDate = items[index++].text;
+  const eventName2 = items[index++].text;
+  const eventLocation = items[index++].text;
+  const documentName = items[index++].text;
+  return [{
+    eventName,
+    eventDate,
+    eventName2,
+    eventLocation,
+    documentName,
+  },
+  index,
+  ];
 }
 
-function convertDriverItemsIntoLaps(driverItems, driver) {
-    const columns = splitIntoColumns(driverItems, driver);
-    if (columns.length != 2) {
-        throw new Error(`Expected 2 columns of laps but got ${columns.length}`);
-    }
-    const col1Laps = splitIntoLaps(columns[0]);
-    const col2Laps = splitIntoLaps(columns[1]);
-    if (col1Laps.length !== col2Laps.length) {
-        throw new Error(`Columns have different number of laps ${col1Laps.length} and ${col2Laps.length}`);
+function readDriverItems(items, startIndex) {
+  let index = startIndex;
+  const driverItems = [];
+  let driverY = items[index].y;
+  while (index < items.length) {
+    let nextItem = items[index];
+
+    if (nextItem.y < driverY) {
+      // page break
+      driverY = nextItem.y;
+      const [, updatedIndex] = readPageHeader(items, index);
+      index = updatedIndex;
+      nextItem = items[index];
     }
 
-    return col1Laps.concat(col2Laps);
+    const nextItemCode = nextItem.R[0].S;
+    if (nextItemCode === -1) {
+      // next driver
+      break;
+    } else if (nextItemCode === 2) {
+      // skip
+      index++;
+    } else if (nextItemCode !== 51 && nextItemCode !== 52) {
+      throw new Error(`Unexpected driver item code in ${JSON.stringify(nextItem)}`);
+    } else {
+      driverItems.push(nextItem);
+      index++;
+    }
+  }
+  return [driverItems, index];
 }
 
 function splitIntoColumns(driverItems, driver) {
-    const FUDGE_FACTOR = 1;
-    const col1 = [];
-    const col2 = [];
-    driverItems.forEach(item => {
-        if (item.x >= driver.columnHeaders[1].lap.x - FUDGE_FACTOR) {
-            col2.push(item);
-        } else if (item.x >= driver.columnHeaders[0].lap.x - FUDGE_FACTOR) {
-            col1.push(item);
-        } else {
-            throw new Error(`Driver item left of first column at ${driver.columnHeaders[0].lap.x}: ${JSON.stringify(item)}`);
-        }
-    });
-    return [col1, col2];
-}
-
-function splitIntoLaps(driverItems) {
-    const uniqueYValues = [...new Set(driverItems.map(item => item.y))];
-    uniqueYValues.sort((a,b) => a-b);
-    const yValueMap = {};
-    uniqueYValues.forEach((value, index) => {
-        yValueMap[value] = index;
-    });
-
-    const lapItems = [...Array(uniqueYValues.length).keys()].map(i => []);
-    driverItems.forEach(item => {
-        lapItems[yValueMap[item.y]].push(item);
-    });
-    
-    return lapItems.map(lap => makeLapFromItems(lap));
+  const FUDGE_FACTOR = 1;
+  const col1 = [];
+  const col2 = [];
+  driverItems.forEach((item) => {
+    if (item.x >= driver.columnHeaders[1].lap.x - FUDGE_FACTOR) {
+      col2.push(item);
+    } else if (item.x >= driver.columnHeaders[0].lap.x - FUDGE_FACTOR) {
+      col1.push(item);
+    } else {
+      throw new Error(`Driver item left of first column at ${driver.columnHeaders[0].lap.x}: ${JSON.stringify(item)}`);
+    }
+  });
+  return [col1, col2];
 }
 
 function makeLapFromItems(lapItems) {
-    lapItems.sort((a, b) => a.x - b.x);
-    if (lapItems.length == 9) {
-        return {
-            'lapNumber': Number(lapItems[0].text),
-            's1': Number(lapItems[1].text),
-            's1Speed': Number(lapItems[2].text),
-            's2': Number(lapItems[3].text),
-            's2Speed': Number(lapItems[4].text),
-            's3': Number(lapItems[5].text),
-            's3Speed': Number(lapItems[6].text),
-            'speedTrap': Number(lapItems[7].text),
-            'laptime': lapItems[8].text,
-        }
-    } else if (lapItems.length == 8) {
-        return {
-            'lapNumber': Number(lapItems[0].text),
-            's1Speed': Number(lapItems[1].text),
-            's2': Number(lapItems[2].text),
-            's2Speed': Number(lapItems[3].text),
-            's3': Number(lapItems[4].text),
-            's3Speed': Number(lapItems[5].text),
-            'speedTrap': Number(lapItems[6].text),
-            'laptime': lapItems[7].text,
-        }
-    } else if (lapItems.length == 1) {
-        return {
-            'lapNumber': Number(lapItems[0].text),
-        }
-    }
-    throw new Error(`Cannot make lap from ${lapItems.length} items: ${JSON.stringify(lapItems, null, 2)}`);
+  lapItems.sort((a, b) => a.x - b.x);
+  if (lapItems.length === 9) {
+    return {
+      lapNumber: Number(lapItems[0].text),
+      s1: Number(lapItems[1].text),
+      s1Speed: Number(lapItems[2].text),
+      s2: Number(lapItems[3].text),
+      s2Speed: Number(lapItems[4].text),
+      s3: Number(lapItems[5].text),
+      s3Speed: Number(lapItems[6].text),
+      speedTrap: Number(lapItems[7].text),
+      laptime: lapItems[8].text,
+    };
+  } if (lapItems.length === 8) {
+    return {
+      lapNumber: Number(lapItems[0].text),
+      s1Speed: Number(lapItems[1].text),
+      s2: Number(lapItems[2].text),
+      s2Speed: Number(lapItems[3].text),
+      s3: Number(lapItems[4].text),
+      s3Speed: Number(lapItems[5].text),
+      speedTrap: Number(lapItems[6].text),
+      laptime: lapItems[7].text,
+    };
+  } if (lapItems.length === 1) {
+    return {
+      lapNumber: Number(lapItems[0].text),
+    };
+  }
+  throw new Error(`Cannot make lap from ${lapItems.length} items: ${JSON.stringify(lapItems, null, 2)}`);
 }
 
-export { parse, splitIntoColumns, splitIntoLaps, makeLapFromItems, convertDriverItemsIntoLaps, readDriverHeader, readDriverItems, parseDriverLaps };
+function splitIntoLaps(driverItems) {
+  const uniqueYValues = [...new Set(driverItems.map((item) => item.y))];
+  uniqueYValues.sort((a, b) => a - b);
+  const yValueMap = {};
+  uniqueYValues.forEach((value, index) => {
+    yValueMap[value] = index;
+  });
+
+  const lapItems = [...Array(uniqueYValues.length).keys()].map(() => []);
+  driverItems.forEach((item) => {
+    lapItems[yValueMap[item.y]].push(item);
+  });
+
+  return lapItems.map((lap) => makeLapFromItems(lap));
+}
+
+function convertDriverItemsIntoLaps(driverItems, driver) {
+  const columns = splitIntoColumns(driverItems, driver);
+  if (columns.length !== 2) {
+    throw new Error(`Expected 2 columns of laps but got ${columns.length}`);
+  }
+  const col1Laps = splitIntoLaps(columns[0]);
+  const col2Laps = splitIntoLaps(columns[1]);
+  if (col1Laps.length !== col2Laps.length) {
+    throw new Error(`Columns have different number of laps ${col1Laps.length} and ${col2Laps.length}`);
+  }
+
+  return col1Laps.concat(col2Laps);
+}
+
+function parseDriverLaps(filename) {
+  return parse(filename)
+    .then((items) => {
+      const doc = {
+        raceTitle: items[0].text,
+        raceDate: items[1].text,
+        raceTitle2: items[2].text,
+        location: items[3].text,
+        doc: items[4].text,
+        drivers: [],
+      };
+
+      let index = 5;
+      while (index < items.length) {
+        const [driver, indexAfterDriverHeader] = readDriverHeader(items, index);
+        index = indexAfterDriverHeader;
+
+        const [driverItems, indexAfterDriverItems] = readDriverItems(items, index);
+        index = indexAfterDriverItems;
+
+        driver.laps = convertDriverItemsIntoLaps(driverItems, driver);
+        doc.drivers.push(driver);
+        console.log(`Successfully added ${driver.name} with ${driver.laps.length} laps`);
+      }
+
+      return doc;
+    });
+}
+
+export {
+  parse, splitIntoColumns, splitIntoLaps, makeLapFromItems, convertDriverItemsIntoLaps,
+  readDriverHeader, readDriverItems, parseDriverLaps,
+};
--- pdfparser/pdfparser.test.js
+++ pdfparser/pdfparser.test.js
@@ -1,236 +1,238 @@
 import fs from 'fs';
-import { splitIntoColumns, splitIntoLaps, makeLapFromItems, convertDriverItemsIntoLaps, readDriverHeader, readDriverItems, parseDriverLaps } from './pdfparser';
+import {
+  splitIntoColumns, splitIntoLaps, makeLapFromItems, convertDriverItemsIntoLaps, readDriverHeader,
+  readDriverItems, parseDriverLaps,
+} from './pdfparser';
 
 test('parsing porsche sprint challenge middle east', async () => {
-    const document = await parseDriverLaps('./sample_data/2022-02-12-Yas Marina/Porsche Sprint Challenge Middle East - Race 1 - Laps and Sectortimes.pdf');
+  const document = await parseDriverLaps('./sample_data/2022-02-12-Yas Marina/Porsche Sprint Challenge Middle East - Race 1 - Laps and Sectortimes.pdf');
 
-    expect(document.raceTitle).toBe('Porsche Sprint Challenge Middle East');
-    expect(document.raceDate).toBe('12 - 13 February 2022')
-    expect(document.raceTitle2).toBe('Porsche Sprint Challenge Middle East');
-    expect(document.location).toBe('Yas Marina Circuit - 5281mtr.');
+  expect(document.raceTitle).toBe('Porsche Sprint Challenge Middle East');
+  expect(document.raceDate).toBe('12 - 13 February 2022');
+  expect(document.raceTitle2).toBe('Porsche Sprint Challenge Middle East');
+  expect(document.location).toBe('Yas Marina Circuit - 5281mtr.');
 
-    expect(document.drivers[0].name).toBe('Morris Schuring');
-    expect(document.drivers[0].number).toBe(1);
-    expect(document.drivers[0].category).toBe('Porsche GT3');
+  expect(document.drivers[0].name).toBe('Morris Schuring');
+  expect(document.drivers[0].number).toBe(1);
+  expect(document.drivers[0].category).toBe('Porsche GT3');
 
-    expect(document.drivers[0].laps[0]).toEqual({
-        lapNumber: 1,
-        s1: undefined,
-        s1Speed: 217.7,
-        s2: 47.866,
-        s2Speed: 220,
-        s3: 43.868,
-        s3Speed: 165.1,
-        speedTrap: 238.9,
-        laptime: '2:00.140'
-    });
-    expect(document.drivers[0].laps[6]).toEqual({
-        lapNumber: 7,
-        s1: 24.969,
-        s1Speed: 221.3,
-        s2: 47.990,
-        s2Speed: 224.1,
-        s3: 43.738,
-        s3Speed: 164.6,
-        speedTrap: 231.8,
-        laptime: '1:56.697'
-    });
-
+  expect(document.drivers[0].laps[0]).toEqual({
+    lapNumber: 1,
+    s1: undefined,
+    s1Speed: 217.7,
+    s2: 47.866,
+    s2Speed: 220,
+    s3: 43.868,
+    s3Speed: 165.1,
+    speedTrap: 238.9,
+    laptime: '2:00.140',
+  });
+  expect(document.drivers[0].laps[6]).toEqual({
+    lapNumber: 7,
+    s1: 24.969,
+    s1Speed: 221.3,
+    s2: 47.990,
+    s2Speed: 224.1,
+    s3: 43.738,
+    s3Speed: 164.6,
+    speedTrap: 231.8,
+    laptime: '1:56.697',
+  });
 });
 
 test('splits into columns with lap 1 sector 1 time present', () => {
-    const driverItems = [...Array(9*6).keys()].map(i => ({
-        text: String(i),
-        x: Math.floor(i/27)*10,
-    }));
-    const driver = {
-        columnHeaders: [
-            {
-                lap: { x: 0 },
-            },
-            {
-                lap: { x: 10 },
-            },
-        ]
-    };
+  const driverItems = [...Array(9 * 6).keys()].map((i) => ({
+    text: String(i),
+    x: Math.floor(i / 27) * 10,
+  }));
+  const driver = {
+    columnHeaders: [
+      {
+        lap: { x: 0 },
+      },
+      {
+        lap: { x: 10 },
+      },
+    ],
+  };
 
-    const columns = splitIntoColumns(driverItems, driver);
-    
-    expect(columns.length).toBe(2);
-    expect(columns[0].length).toBe(9*3);
-    expect(columns[1].length).toBe(9*3);
-    expect(columns[0][0].text).toBe('0');
-    expect(columns[0][26].text).toBe('26');
-    expect(columns[1][0].text).toBe('27');
-    expect(columns[1][26].text).toBe('53');
-})
+  const columns = splitIntoColumns(driverItems, driver);
+
+  expect(columns.length).toBe(2);
+  expect(columns[0].length).toBe(9 * 3);
+  expect(columns[1].length).toBe(9 * 3);
+  expect(columns[0][0].text).toBe('0');
+  expect(columns[0][26].text).toBe('26');
+  expect(columns[1][0].text).toBe('27');
+  expect(columns[1][26].text).toBe('53');
+});
 
 test('splits into columns with lap 1 sector 1 time missing', () => {
-    const driverItems = [...Array(9*6).keys()].slice(1).map(i => ({
-        text: String(i),
-        x: Math.floor(i/27)*10,
-    }));
-    const driver = {
-        columnHeaders: [
-            {
-                lap: { x: 0 },
-            },
-            {
-                lap: { x: 10 },
-            },
-        ]
-    };
+  const driverItems = [...Array(9 * 6).keys()].slice(1).map((i) => ({
+    text: String(i),
+    x: Math.floor(i / 27) * 10,
+  }));
+  const driver = {
+    columnHeaders: [
+      {
+        lap: { x: 0 },
+      },
+      {
+        lap: { x: 10 },
+      },
+    ],
+  };
 
-    const columns = splitIntoColumns(driverItems, driver);
-    
-    expect(columns.length).toBe(2);
-    expect(columns[0].length).toBe((9*3) - 1);
-    expect(columns[1].length).toBe(9*3);
-    expect(columns[0][0].text).toBe('1');
-    expect(columns[0][25].text).toBe('26');
-    expect(columns[1][0].text).toBe('27');
-    expect(columns[1][26].text).toBe('53');
-})
+  const columns = splitIntoColumns(driverItems, driver);
+
+  expect(columns.length).toBe(2);
+  expect(columns[0].length).toBe((9 * 3) - 1);
+  expect(columns[1].length).toBe(9 * 3);
+  expect(columns[0][0].text).toBe('1');
+  expect(columns[0][25].text).toBe('26');
+  expect(columns[1][0].text).toBe('27');
+  expect(columns[1][26].text).toBe('53');
+});
 
 test('splits into laps', () => {
-    const driverItems = [...Array(9*3).keys()].map(i => ({
-            x: i % 9,
-            y: Math.floor(i / 9),
-            text: String(i),
-        }));
+  const driverItems = [...Array(9 * 3).keys()].map((i) => ({
+    x: i % 9,
+    y: Math.floor(i / 9),
+    text: String(i),
+  }));
 
-    const laps = splitIntoLaps(driverItems);
+  const laps = splitIntoLaps(driverItems);
 
-    expect(laps.length).toBe(3);
+  expect(laps.length).toBe(3);
 
-    expect(laps[0]).toEqual({
-        lapNumber: 0,
-        s1: 1,
-        s1Speed: 2,
-        s2: 3,
-        s2Speed: 4,
-        s3: 5,
-        s3Speed: 6,
-        speedTrap: 7,
-        laptime: '8'
-    });
+  expect(laps[0]).toEqual({
+    lapNumber: 0,
+    s1: 1,
+    s1Speed: 2,
+    s2: 3,
+    s2Speed: 4,
+    s3: 5,
+    s3Speed: 6,
+    speedTrap: 7,
+    laptime: '8',
+  });
 });
 
 test('make lap from 9 items', () => {
-    const lapItems = [...Array(9).keys()].map(i => ({
-        x: i,
-        y: 10,
-        text: String(i),
-    }));
+  const lapItems = [...Array(9).keys()].map((i) => ({
+    x: i,
+    y: 10,
+    text: String(i),
+  }));
 
-    const lap = makeLapFromItems(lapItems);
+  const lap = makeLapFromItems(lapItems);
 
-    expect(lap).toEqual({
-        lapNumber: 0,
-        s1: 1,
-        s1Speed: 2,
-        s2: 3,
-        s2Speed: 4,
-        s3: 5,
-        s3Speed: 6,
-        speedTrap: 7,
-        laptime: '8',
-    });
+  expect(lap).toEqual({
+    lapNumber: 0,
+    s1: 1,
+    s1Speed: 2,
+    s2: 3,
+    s2Speed: 4,
+    s3: 5,
+    s3Speed: 6,
+    speedTrap: 7,
+    laptime: '8',
+  });
 });
 
 test('make lap from 8 items', () => {
-    const lapItems = [...Array(8).keys()].map(i => ({
-        x: i,
-        y: 10,
-        text: String(i),
-    }));
+  const lapItems = [...Array(8).keys()].map((i) => ({
+    x: i,
+    y: 10,
+    text: String(i),
+  }));
 
-    const lap = makeLapFromItems(lapItems);
+  const lap = makeLapFromItems(lapItems);
 
-    expect(lap).toEqual({
-        lapNumber: 0,
-        s1: undefined,
-        s1Speed: 1,
-        s2: 2,
-        s2Speed: 3,
-        s3: 4,
-        s3Speed: 5,
-        speedTrap: 6,
-        laptime: '7',
-    });
+  expect(lap).toEqual({
+    lapNumber: 0,
+    s1: undefined,
+    s1Speed: 1,
+    s2: 2,
+    s2Speed: 3,
+    s3: 4,
+    s3Speed: 5,
+    speedTrap: 6,
+    laptime: '7',
+  });
 });
 
 test('reads driver header', () => {
-    const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_items.json'));
+  const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_items.json'));
 
-    const [ driver, index ] = readDriverHeader(items, 0);
+  const [driver, index] = readDriverHeader(items, 0);
 
-    expect(index).toBe(13);
-    expect(driver).toEqual({
-        name: 'Daan van Kuijk',
-        number: 8,
-        category: 'Porsche GT3',
-        columnHeaders: [
-            {
-                lap: { x: 1.297 },
-            },
-            {
-                lap: { x: 18.641 },
-            },
-        ],
-    });
+  expect(index).toBe(13);
+  expect(driver).toEqual({
+    name: 'Daan van Kuijk',
+    number: 8,
+    category: 'Porsche GT3',
+    columnHeaders: [
+      {
+        lap: { x: 1.297 },
+      },
+      {
+        lap: { x: 18.641 },
+      },
+    ],
+  });
 });
 
 test('reads driver items', () => {
-    const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_items.json'));
+  const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_items.json'));
 
-    const [ driver, index ] = readDriverHeader(items, 0);
-    const [ driverItems, endIndex ] = readDriverItems(items, index);
+  const [, index] = readDriverHeader(items, 0);
+  const [, endIndex] = readDriverItems(items, index);
 
-    expect(endIndex).toBe(120);
-    expect(items[endIndex].text).toBe(' Andrey Mukovoz');
+  expect(endIndex).toBe(120);
+  expect(items[endIndex].text).toBe(' Andrey Mukovoz');
 });
 
 test('converts a driver items into laps', () => {
-    const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_items.json'));
+  const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_items.json'));
 
-    const [ driver, index ] = readDriverHeader(items, 0);
-    const [ driverItems, endIndex ] = readDriverItems(items, index);
-    const laps = convertDriverItemsIntoLaps(driverItems, driver);
-    
-    expect(laps.length).toBe(12);
+  const [driver, index] = readDriverHeader(items, 0);
+  const [driverItems] = readDriverItems(items, index);
+  const laps = convertDriverItemsIntoLaps(driverItems, driver);
+
+  expect(laps.length).toBe(12);
 });
 
 test('reads driver items over a page', () => {
-    const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_items_with_page_break.json'));
+  const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_items_with_page_break.json'));
 
-    const [ driver, index ] = readDriverHeader(items, 0);
-    const [ driverItems, endIndex ] = readDriverItems(items, index);
+  const [, index] = readDriverHeader(items, 0);
+  const [, endIndex] = readDriverItems(items, index);
 
-    expect(endIndex).toBe(129);
-    expect(items[endIndex].text).toBe(' Lucas Groeneveld');
+  expect(endIndex).toBe(129);
+  expect(items[endIndex].text).toBe(' Lucas Groeneveld');
 });
 
 test('converts driver with empty lap into laps', () => {
-    const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_missing_lap.json'));
+  const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_missing_lap.json'));
 
-    const [ driver, index ] = readDriverHeader(items, 0);
-    const [ driverItems, endIndex ] = readDriverItems(items, index);
-    const laps = convertDriverItemsIntoLaps(driverItems, driver);
+  const [driver, index] = readDriverHeader(items, 0);
+  const [driverItems, endIndex] = readDriverItems(items, index);
+  const laps = convertDriverItemsIntoLaps(driverItems, driver);
 
-    expect(endIndex).toBe(122);
-    expect(laps.length).toBe(12);
+  expect(endIndex).toBe(122);
+  expect(laps.length).toBe(12);
 });
 
 // next test: take page break driver items and split into columns
 // column splitting does not work over a page break
 
 test('converts a second driver items into laps', () => {
-    const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_items_with_page_break.json'));
+  const items = JSON.parse(fs.readFileSync('sample_data/2022-02-12-Yas Marina/driver_items_with_page_break.json'));
 
-    const [ driver, index ] = readDriverHeader(items, 0);
-    const [ driverItems, endIndex ] = readDriverItems(items, index);
-    const laps = convertDriverItemsIntoLaps(driverItems, driver);
-    
-    expect(laps.length).toBe(12);
+  const [driver, index] = readDriverHeader(items, 0);
+  const [driverItems] = readDriverItems(items, index);
+  const laps = convertDriverItemsIntoLaps(driverItems, driver);
+
+  expect(laps.length).toBe(12);
 });
